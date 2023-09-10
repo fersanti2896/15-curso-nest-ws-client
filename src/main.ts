aa067520a5ed94555890487a5cf5cf208cdf4e59
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-09 22:32:50 -0600

    Definiendo estructura al recibir lo que emite el servidor en el cliente.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>WebSocket Client</h1>
-    <span>Offline</span>
+    <span id="server-status">Offline</span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,6 +1,61 @@
-import { Manager } from 'socket.io-client'
+import { Manager, Socket } from 'socket.io-client'
 
 export const connectToServer = () => {
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
     const socket = manager.socket('/');
+    addListener( socket );
+}
+
+const addListener = ( socket: Socket ) => {
+    const clientsUL = document.querySelector<HTMLUListElement>('#clients-ul');
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form');
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input');
+    const messagesUL = document.querySelector<HTMLUListElement>('#messages-ul');
+    const serverStatusLabel = document.querySelector('#server-status');
+
+    socket.on('connect', () => {
+        serverStatusLabel!.innerHTML = 'Conectado';
+    });
+
+    socket.on('disconnect', () => {
+        serverStatusLabel!.innerHTML = 'Desconectado';
+    });
+
+    socket.on('clients-updated', ( clients: string[] ) => {
+        let clientsHTML = '';
+
+        clients.forEach( clientId => {
+            clientsHTML += `
+                <li>${ clientId }</li>
+            `
+        } );
+
+        clientsUL!.innerHTML = clientsHTML;
+    });
+
+    messageForm?.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if( messageInput!.value.trim().length <= 0 ) return;
+
+        socket.emit('message-from-client', {
+            id: 'Yo',
+            message: messageInput!.value
+        });
+
+        messageInput!.value = '';
+    });
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const newMessage = `
+            <li>
+                <strong>${ payload.fullName }</strong>
+                <span>${ payload.message }</span>
+            </li>
+        `;
+
+        const li = document.createElement('li');
+        li.innerHTML = newMessage;
+        messagesUL?.append(li);
+    });
 }
